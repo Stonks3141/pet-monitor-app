8b7c8225b00b5f80fcd62de2b045698397e118a0
Sam Nystrom <samuel.l.nystrom@gmail.com>
2022-08-15 13:28:51 -0400

    Move main function to lib.rs

--- pet-monitor-app/src/lib.rs
+++ pet-monitor-app/src/lib.rs
@@ -12,3 +12,33 @@ pub mod secrets;
 mod stream;
 #[cfg(test)]
 mod tests;
+
+use ring::rand::SystemRandom;
+use routes::*;
+
+/// The main function for the program. This is a library function to make unit
+/// and integration testing easier.
+/// 
+/// # Example
+/// ```no_test
+/// use rocket::local::blocking::Client;
+/// use rocket::http::Status;
+/// 
+/// let client = Client::tracked(pet_monitor_app::rocket().await).unwrap();
+/// let res = client.get("/api/auth").dispatch();
+/// ```
+pub async fn rocket() -> rocket::Rocket<rocket::Build> {
+    let rng = SystemRandom::new();
+    let pwd = secrets::Password::new(&rng)
+        .await
+        .expect("Failed to initialize password.");
+    let secret = secrets::Secret::new(&rng)
+        .await
+        .expect("Failed to initialize JWT secret.");
+
+    rocket::build()
+        .mount("/", rocket::routes![login, verify])
+        .manage(pwd)
+        .manage(secret)
+        .manage(rng)
+}
--- pet-monitor-app/src/main.rs
+++ pet-monitor-app/src/main.rs
@@ -1,21 +1,4 @@
-use pet_monitor_app::routes::*;
-use pet_monitor_app::secrets;
-use ring::rand::SystemRandom;
-use rocket::{launch, routes};
-
-#[launch]
+#[rocket::launch]
 async fn rocket() -> _ {
-    let rng = SystemRandom::new();
-    let pwd = secrets::Password::new(&rng)
-        .await
-        .expect("Failed to initialize password.");
-    let secret = secrets::Secret::new(&rng)
-        .await
-        .expect("Failed to initialize JWT secret.");
-
-    rocket::build()
-        .mount("/", routes![login, verify])
-        .manage(pwd)
-        .manage(secret)
-        .manage(rng)
+    pet_monitor_app::rocket().await
 }
