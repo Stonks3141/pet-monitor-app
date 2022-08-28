d62701a87989a1147696e7ceeb359081af919804
Sam Nystrom <82178396+Stonks3141@users.noreply.github.com>
2022-08-28 14:00:23 -0400

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 git update-index --refresh
-if [ git diff-index --quiet HEAD -- ]; then
+if [ $(git diff-index --quiet HEAD --) = 0 ]; then
   tag=$(git log -1 --pretty=%H)
 else
   tag="latest"
 fi
 
-if [ -t 1 ]; then; echo "Building frontend container..."; fi
-docker build ./client -t pet-monitor-app-client:test-$tag --target base
-if [ -t 1 ]; then; echo "Testing frontend..."; fi
-docker run --workdir /usr/local/src/pet-monitor-app pet-monitor-app-client:test-$tag "pnpm test"
+# [ -t 1 ] && echo "Building frontend container..."
+# docker build ./client -t pet-monitor-app-client:test-$tag --target base
+# [ -t 1 ] && echo "Testing frontend..."
+# docker run --workdir /usr/local/src/pet-monitor-app pet-monitor-app-client:test-$tag "pnpm test"
 
-if [ -t 1 ]; then; echo "Building server container..."; fi
+[ -t 1 ] && echo "Building server container..."
 docker build ./pet-monitor-app -t pet-monitor-app:test-$tag --target base
-if [ -t 1 ]; then; echo "Testing server..."; fi
+[ -t 1 ] && echo "Testing server..."
 docker run --workdir /usr/local/src/pet-monitor-app pet-monitor-app:test-$tag "cargo test"
 
-if [ -t 1 ]; then; echo "Tests complete!"; fi
+[ -t 1 ] && echo "Tests complete!"
