e34344e4ee12fdafb4444fd7b03ebdf329bd1ef5
Sam Nystrom <82178396+Stonks3141@users.noreply.github.com>
2022-08-28 13:18:49 -0400

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,12 +7,16 @@ else
   tag="latest"
 fi
 
-echo "Building frontend bundle..."
+[ -t 1 ] && echo "Building frontend bundle..."
 docker build ./client -t pet-monitor-app-client:$tag
-echo "Copying bundle out of container..."
+[ -t 1 ] && echo "Copying bundle out of container..."
 docker cp pet-monitor-app-client:$tag:/usr/local/src/pet-monitor-app/dist ./client
-echo "Building server..."
+[ -t 1 ] && echo "Building server..."
 docker build ./pet-monitor-app -t pet-monitor-app:$tag
 
+if [ -t 1 ]; then
 echo "Build complete! Run with \
 `docker run -it pet-monitor-app:$tag`."
+else
+  echo "pet-monitor-app:$tag"
+fi
