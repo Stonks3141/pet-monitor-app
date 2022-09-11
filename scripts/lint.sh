6c1c7043d88dc6fedbcc80ac3e180b1be96df268
Sam Nystrom <82178396+Stonks3141@users.noreply.github.com>
2022-09-10 20:14:13 -0400

    Add lint script

---
+++ scripts/lint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+git update-index --refresh
+if [ $(git diff-index --quiet HEAD --) = 0 ]; then
+  tag=$(git log -1 --pretty=%H)
+else
+  tag="latest"
+fi
+
+echo "Building server container..."
+docker build ./pet-monitor-app -t pet-monitor-app:test-$tag --target base
+echo "Linting server..."
+docker run \
+--workdir /usr/local/src/pet-monitor-app pet-monitor-app:test-$tag \
+--mount type=bind,src=./pet-monitor-app,dst=/usr/local/src/pet-monitor-app \
+"cargo clippy"
+
+echo "Linting complete!"
