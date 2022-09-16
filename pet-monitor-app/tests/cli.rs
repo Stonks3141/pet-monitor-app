c604c388d637f713e386937ad52a580b9f3e54d8
Sam Nystrom <82178396+Stonks3141@users.noreply.github.com>
2022-09-15 23:37:52 -0400

    Add integration tests for CLI

---
+++ pet-monitor-app/tests/cli.rs
@@ -0,0 +1,48 @@
+use predicates::prelude::*;
+use assert_fs::{prelude::*, NamedTempFile};
+use assert_cmd::prelude::*;
+
+#[test]
+fn create_config() {
+    let tmp = NamedTempFile::new("pet-monitor-app.toml").unwrap();
+    Command::cargo_bin("pet-monitor-app")
+        .unwrap()
+        .arg("--config")
+        .arg(tmp.path())
+        .arg("configure")
+        .assert()
+        .success();
+    tmp.assert(predicate::path::exists());
+    tmp.assert(predicate::path::is_file());
+}
+
+#[test]
+fn set_password() {
+    let tmp = NamedTempFile::new("pet-monitor-app.toml").unwrap();
+    Command::cargo_bin("pet-monitor-app")
+        .unwrap()
+        .arg("--config")
+        .arg(tmp.path())
+        .arg("configure")
+        .arg("--password")
+        .arg("123")
+        .assert()
+        .success();
+    tmp.assert(predicate::path::exists());
+    tmp.assert(predicate::path::is_file());
+}
+
+#[test]
+fn regen_secret() {
+    let tmp = NamedTempFile::new("pet-monitor-app.toml").unwrap();
+    Command::cargo_bin("pet-monitor-app")
+        .unwrap()
+        .arg("--config")
+        .arg(tmp.path())
+        .arg("configure")
+        .arg("--regen-secret")
+        .assert()
+        .success();
+    tmp.assert(predicate::path::exists());
+    tmp.assert(predicate::path::is_file());
+}
