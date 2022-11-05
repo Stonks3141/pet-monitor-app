f27ae6f044b51b00b279f6dd38919cf5a78dc23a
Sam <samuel.l.nystrom@gmail.com>
2022-11-05 14:56:25 -0400

    client: style stream route

--- client/src/routes/stream/+page.svelte
+++ client/src/routes/stream/+page.svelte
@@ -4,6 +4,7 @@
   let [width, height] = data.resolution;
 </script>
 
+<a href="/config" class="btn m-4 absolute top-0 right-0">Settings</a>
 <video controls autoplay muted playsinline {width} {height}>
   <source src="/stream.mp4" type="video/mp4" />
 </video>
