27b913ae6688fcde752d539777415b93f34e0974
Sam <samuel.l.nystrom@gmail.com>
2022-11-13 14:58:20 -0500

    client: remove `console.log`

--- client/src/routes/stream/+page.ts
+++ client/src/routes/stream/+page.ts
@@ -3,7 +3,6 @@ import type { PageLoad } from './$types';
 export const load: PageLoad = async ({ fetch }) => {
   const res = await fetch('/api/config');
   const config = await res.json();
-  console.log(config);
   return {
     resolution: config.resolution,
   };
