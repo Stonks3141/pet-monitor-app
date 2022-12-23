10ccf232a2281a325c597581f3f54ec85c79d8bd
Sam Nystrom <samuel.l.nystrom@gmail.com>
2022-12-22 20:14:02 -0500

    create flake.nix

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A simple and secure pet monitor for Linux.";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
+  };
+  outputs = inputs@{ self, nixpkgs, utils, ... }: utils.lib.mkFlake {
+    inherit self inputs;
+    outputsBuilder = channels:
+      let pkgs = channels.nixpkgs; in {
+        devShell = pkgs.mkShell {
+          name = "pet-monitor-app";
+          packages = with pkgs; [
+            cargo
+            rustfmt
+            libclang
+            pkg-config
+            x264
+            nodePackages.pnpm
+            nixpkgs-fmt
+          ];
+          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+          BINDGEN_EXTRA_CLANG_ARGS = [
+            "-I${pkgs.libclang.lib}/lib/clang/${pkgs.libclang.version}/include"
+            "-I${pkgs.glibc.dev}/include"
+          ];
+        };
+      };
+  };
+}
