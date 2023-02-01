a65e62892a0c2d2cd236401bb6bbb7c11f74d9b6
Sam Nystrom <samuel.l.nystrom@gmail.com>
2023-02-01 17:43:15 -0500

    ci: use nix for CI

--- flake.nix
+++ flake.nix
@@ -3,13 +3,18 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = inputs@{ self, nixpkgs, utils, ... }: utils.lib.mkFlake {
+  outputs = inputs@{ self, nixpkgs, utils, fenix, ... }: utils.lib.mkFlake {
     inherit self inputs;
-    supportedSystems = [ "x86_64-linux" "arm-linux" ];
+    sharedOverlays = [ fenix.overlays.default ];
+    supportedSystems = [ "x86_64-linux" ];
     outputsBuilder = channels:
       let pkgs = channels.nixpkgs; in {
-        devShell = pkgs.mkShell {
+        devShells.default = pkgs.mkShell {
           name = "pet-monitor-app";
           packages = with pkgs; [
             cargo
@@ -17,7 +22,6 @@
             clippy
             rustfmt
             rust-analyzer
-            cargo-udeps
             just
             cargo-flamegraph
             oha
@@ -33,7 +37,46 @@
             "-I${pkgs.glibc.dev}/include"
           ];
         };
-        defaultPackage = with import nixpkgs { system = "x86_64-linux"; };
+
+        devShells.check = pkgs.mkShell {
+          name = "pet-monitor-app-check";
+          packages = with pkgs; [
+            cargo
+            rustc
+            clippy
+            rustfmt
+            just
+            libclang
+            pkg-config
+            x264
+          ];
+          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+          BINDGEN_EXTRA_CLANG_ARGS = [
+            "-I${pkgs.libclang.lib}/lib/clang/${pkgs.libclang.version}/include"
+            "-I${pkgs.glibc.dev}/include"
+          ];
+        };
+
+        devShells.udeps = pkgs.mkShell {
+          name = "pet-monitor-app-udeps";
+          packages = with pkgs; [
+            (fenix.packages.x86_64-linux.minimal.withComponents [
+              "cargo"
+              "rustc"
+            ])
+            cargo-udeps
+            libclang
+            pkg-config
+            x264
+          ];
+          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+          BINDGEN_EXTRA_CLANG_ARGS = [
+            "-I${pkgs.libclang.lib}/lib/clang/${pkgs.libclang.version}/include"
+            "-I${pkgs.glibc.dev}/include"
+          ];
+        };
+
+        packages.default = with import nixpkgs { system = "x86_64-linux"; };
           rustPlatform.buildRustPackage {
             pname = "pet-monitor-app";
             version = "0.3.0";
