b1726ba645e30bd3503092e229aebf7717901b9b
Sam Nystrom <samuel.l.nystrom@gmail.com>
2023-02-08 20:00:24 -0500

    build(flake): use numtide/flake-utils

--- flake.nix
+++ flake.nix
@@ -2,19 +2,17 @@
   description = "A simple and secure pet monitor for Linux.";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
+    utils.url = "github:numtide/flake-utils";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = inputs@{ self, nixpkgs, utils, fenix, ... }: utils.lib.mkFlake {
-    inherit self inputs;
-    sharedOverlays = [ fenix.overlays.default ];
-    supportedSystems = [ "x86_64-linux" ];
-    outputsBuilder = channels:
+  outputs = { self, nixpkgs, utils, fenix, ... }:
+    utils.lib.eachSystem (with utils.lib.system; [ x86_64-linux ]) (system:
       let
-        pkgs = channels.nixpkgs;
+        # system = utils.lib.system.x86_64-linux;
+        pkgs = nixpkgs.legacyPackages.${system};
         mkShell = { name, packages }: pkgs.mkShell {
           inherit name;
           packages = with pkgs; [
@@ -28,7 +26,10 @@
             "-I${pkgs.glibc.dev}/include"
           ];
         };
-      in {
+      in
+      {
+        overlays.default = _self: _super: fenix.overlays.default;
+
         devShells.default = mkShell {
           name = "pet-monitor-app";
           packages = with pkgs; [
@@ -66,26 +67,24 @@
           ];
         };
 
-        packages.default = with import nixpkgs { system = "x86_64-linux"; };
-          rustPlatform.buildRustPackage {
-            pname = "pet-monitor-app";
-            version = "0.3.0";
-            src = ./.;
-            cargoLock.lockFile = ./Cargo.lock;
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "pet-monitor-app";
+          version = "0.3.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
 
-            nativeBuildInputs = [
-              libclang
-              pkg-config
-              x264
-            ];
-            buildInputs = [ pkg-config x264 ];
+          nativeBuildInputs = with pkgs; [
+            libclang
+            pkg-config
+            x264
+          ];
 
-            LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-            BINDGEN_EXTRA_CLANG_ARGS = [
-              "-I${pkgs.libclang.lib}/lib/clang/${pkgs.libclang.version}/include"
-              "-I${pkgs.glibc.dev}/include"
-            ];
-          };
-      };
-  };
+          LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+          BINDGEN_EXTRA_CLANG_ARGS = [
+            "-I${pkgs.libclang.lib}/lib/clang/${pkgs.libclang.version}/include"
+            "-I${pkgs.glibc.dev}/include"
+          ];
+        };
+      }
+    );
 }
