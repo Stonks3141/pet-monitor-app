963822e1a45c6ee9fdedb746695ec9e436cd0d2f
Sam Nystrom <samuel.l.nystrom@gmail.com>
2023-02-11 14:21:24 -0500

    refactor: replace clap with xflags

--- crates/pet-monitor-app/src/bin/pet-monitor-app/main.rs
+++ crates/pet-monitor-app/src/bin/pet-monitor-app/main.rs
@@ -4,12 +4,10 @@
 #![warn(clippy::unimplemented)]
 #![warn(clippy::dbg_macro)]
 
-mod cli;
-
-use clap::Parser;
 use color_eyre::eyre;
 use pet_monitor_app::config;
 use ring::rand::{SecureRandom, SystemRandom};
+use std::path::PathBuf;
 use tokio::task::spawn_blocking;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry};
 use tracing_tree::HierarchicalLayer;
@@ -43,7 +41,43 @@ const ARGON2_CONFIG: argon2::Config = argon2::Config {
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     color_eyre::install()?;
-    let cmd = cli::Cmd::parse();
+
+    xflags::xflags! {
+        cmd pet-monitor-app {
+            /// Print version
+            optional -V, --version
+            /// Path to the configuration file to use
+            optional -c, --config path: PathBuf
+            /// Regenerate the secret used for signing JWTs
+            cmd regen-secret {}
+            /// Set the password
+            cmd set-password {
+                required password: String
+            }
+            /// Start the server
+            cmd start {
+                /// Enable or disable TLS
+                optional --tls tls: bool
+                /// Set the port to listen on for HTTPS
+                optional --tls-port tls_port: u16
+                /// Path to an SSL certificate
+                optional --cert cert: PathBuf
+                /// Path to an SSL certificate key
+                optional --key cert: PathBuf
+                /// Set the port to listen on
+                optional -p, --port port: u16
+                /// Disable video streaming
+                optional --no-stream
+            }
+        }
+    };
+    let cmd = PetMonitorApp::from_env_or_exit();
+
+    if cmd.version {
+        println!("pet-monitor-app {}", env!("CARGO_PKG_VERSION"));
+        return Ok(());
+    }
+
     Registry::default()
         .with(EnvFilter::from_default_env())
         .with(
@@ -56,37 +90,37 @@ async fn main() -> eyre::Result<()> {
 
     let rng = SystemRandom::new();
 
-    let mut ctx = config::load(cmd.conf_path.clone()).await?;
+    let mut ctx = config::load(cmd.config.clone()).await?;
 
     if ctx.jwt_secret == [0; 32] {
         rng.fill(&mut ctx.jwt_secret)?;
-        config::store(cmd.conf_path.clone(), ctx.clone()).await?;
+        config::store(cmd.config.clone(), ctx.clone()).await?;
     }
 
-    match cmd.command {
-        cli::SubCmd::RegenSecret => {
+    match cmd.subcommand {
+        PetMonitorAppCmd::RegenSecret(_) => {
             rng.fill(&mut ctx.jwt_secret)?;
-            config::store(cmd.conf_path.clone(), ctx.clone()).await?;
+            config::store(cmd.config.clone(), ctx.clone()).await?;
             println!("Successfully regenerated JWT signing secret.");
         }
-        cli::SubCmd::SetPassword { password } => {
+        PetMonitorAppCmd::SetPassword(SetPassword { password }) => {
             let mut buf = [0u8; 16];
             rng.fill(&mut buf)?;
             ctx.password_hash = spawn_blocking(move || {
                 argon2::hash_encoded(password.as_bytes(), &buf, &ARGON2_CONFIG)
             })
             .await??;
-            config::store(cmd.conf_path.clone(), ctx.clone()).await?;
+            config::store(cmd.config.clone(), ctx.clone()).await?;
             println!("Successfully reset password.");
         }
-        cli::SubCmd::Start {
-            stream,
+        PetMonitorAppCmd::Start(Start {
+            no_stream,
             tls,
             port,
             tls_port,
             cert,
             key,
-        } => {
+        }) => {
             if let Some(port) = port {
                 ctx.port = port;
             }
@@ -118,7 +152,7 @@ async fn main() -> eyre::Result<()> {
                     _ => (),
                 },
             }
-            pet_monitor_app::start(cmd.conf_path, ctx, stream).await?;
+            pet_monitor_app::start(cmd.config, ctx, !no_stream).await?;
         }
     }
     Ok(())
