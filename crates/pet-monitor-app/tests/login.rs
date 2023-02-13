c20fcdb8171963205b590e5320286d4ec36696e0
Sam Nystrom <samuel.l.nystrom@gmail.com>
2023-02-13 10:08:30 -0500

    test: add tests for login.html route

---
+++ crates/pet-monitor-app/tests/login.rs
@@ -0,0 +1,26 @@
+mod common;
+
+use common::Cmd;
+
+#[test]
+fn login_correct_password() {
+    Cmd::start()
+        .no_stream()
+        .with_open_port()
+        .with_password("123")
+        .with_request(|r| r.post("/login.html").form("password=123"))
+        .assert()
+        .see_other("/stream.html")
+        .has_valid_token();
+}
+
+#[test]
+fn login_incorrect_password() {
+    Cmd::start()
+        .no_stream()
+        .with_open_port()
+        .with_password("123")
+        .with_request(|r| r.post("/login.html").form("password=foo"))
+        .assert()
+        .see_other("/login.html");
+}
